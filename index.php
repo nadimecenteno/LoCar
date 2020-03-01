70370e9257db9525bd9db5cd88d42334e7cb7bd6
Nadime <nadimecenteno@hotmail.com>
2020-02-29 23:26:55 -0300

    .

--- index.php
+++ index.php
@@ -25,22 +25,7 @@
         <div data-layer="90463dc7-0c9a-42b0-9c3f-277d76ed2363">
             <input class="dataRet" type="date" required="required" maxlength="10" placeholder="dd/mm/aaaa" name="dataRet" 
             pattern="[0-9]{2}\/[0-9]{2}\/[0-9]{4}$" min="" id="dataRet" style="border-color: transparent;">
-            <script type="text/javascript">
-                var today = new Date();
-                var dd = today.getDate();
-                var mm = today.getMonth()+1; //January is 0!
-                var yyyy = today.getFullYear();
-                 if(dd<10){
-                        dd='0'+dd
-                    } 
-                    if(mm<10){
-                        mm='0'+mm
-                    } 
-                
-                today = yyyy+'-'+mm+'-'+dd;
-                document.getElementById("datefield").setAttribute("min", today);
-            </script>
-                
+            
         </div>
         <div data-layer="52a73616-e475-42d0-b2dc-56a12db43756">
             <!-- Modal HTML embedded directly into document -->
